3168ce59f1db170fde285d7c75b962988b9123f6
Alexander Leontev <alexander.leontev@carlsonwagonlit.com>
2019-08-07 11:24:10 +0300

    Remove unneeded synthetic delay

--- GalleryExerciseApp/Data/Gallery Service/Implementation/DefaultGalleryService.swift
+++ GalleryExerciseApp/Data/Gallery Service/Implementation/DefaultGalleryService.swift
@@ -86,7 +86,6 @@ class DefaultGalleryService: GalleryService {
                 
                 return response
             }
-            .delay(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
     }
     
     func image(id: String) -> Observable<UIImage> {
